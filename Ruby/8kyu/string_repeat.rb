107139c926118877621d449f5af3e670b4811e7e
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 20:25:43 +0200

    Add solution for string_repeat.rb

---
+++ Ruby/8kyu/string_repeat.rb
@@ -0,0 +1,20 @@
+# Write a function called repeatStr which repeats the given string string exactly n times.
+#
+# repeatStr(6, "I") // "IIIIII"
+# repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+#
+# def repeat_str (n, s)
+#   #your code here
+# end
+
+# My solution
+
+def repeat_str (n, s)
+  s * n
+end
+
+# The most popular solution
+# 
+# def repeat_str (n, s)
+#   s*n
+# end
