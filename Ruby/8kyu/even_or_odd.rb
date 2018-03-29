31787ef5aa0e4662a0dfa59552c5565c14e4521f
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 19:14:40 +0200

    Add solution for even_or_odd.rb

---
+++ Ruby/8kyu/even_or_odd.rb
@@ -0,0 +1,17 @@
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+#
+# def even_or_odd(number)
+#   # your code
+# end
+
+# My solution
+
+def even_or_odd(number)
+  number.even? ? "Even" : "Odd"
+end
+
+# The most popular solution
+# 
+# def even_or_odd(number)
+#   number.even? ? "Even" : "Odd"
+# end
