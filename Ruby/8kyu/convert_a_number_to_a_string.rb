e92612d95b430032e7949ed8246e2a42b5ced4fd
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 15:31:19 +0200

    Add solution for convert_a_number_to_a_string.rb

---
+++ Ruby/8kyu/convert_a_number_to_a_string.rb
@@ -0,0 +1,24 @@
+# We need a function that can transform a number into a string.
+#
+# What ways of achieving this do you know?
+#
+# Examples:
+# number-to-string 123 ;; returns '123'
+# number-to-string 999 ;; returns '999'
+
+# def numberToString(num)
+#   # Convert a Number to a String
+# end
+
+
+# My solution
+
+def numberToString(num)
+  num.to_s
+end
+
+# The most popular solution
+
+# def numberToString(num)
+#   num.to_s
+# end
