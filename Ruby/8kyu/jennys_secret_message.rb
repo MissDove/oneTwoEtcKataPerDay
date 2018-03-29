8d97969e1dd0069b819e3a02c3e712145f1de0c9
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 19:23:13 +0200

    Add solution for jennys_secret_message.rb

---
+++ Ruby/8kyu/jennys_secret_message.rb
@@ -0,0 +1,20 @@
+# Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+#
+# Can you help her?
+#
+# def greet(name)
+#   return "Hello, #{name}!"
+#   return "Hello, my love!" if name == "Johnny"
+# end
+
+# My solution
+
+def greet(name)
+  name == "Johnny" ? "Hello, my love!" : "Hello, #{name}!"
+end
+
+# The most popular solution
+# 
+# def greet(name)
+#   name == 'Johnny' ? "Hello, my love!" : "Hello, #{name}!"
+# end
