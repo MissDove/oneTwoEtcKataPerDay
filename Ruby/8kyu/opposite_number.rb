7280fc487f588eac140967810a04ebe94f28ffea
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 19:18:47 +0200

    Add solution for opposite_number.rb

---
+++ Ruby/8kyu/opposite_number.rb
@@ -0,0 +1,24 @@
+# Very simple, given a number, find its opposite.
+#
+# Examples:
+#
+# 1: -1
+# 14: -14
+# -34: 34
+# But can you do it in 1 line of code and without any conditionals?
+#
+# def opposite
+#  #your code here
+# end
+
+# My solution
+
+def opposite(num)
+  num = -num
+end
+
+# The most popular solution
+# 
+# def opposite n
+#   -n
+# end
