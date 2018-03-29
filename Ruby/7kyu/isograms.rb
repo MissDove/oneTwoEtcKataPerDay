1e6755bde13f76b00c00a55c275abf21705a8273
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 15:42:50 +0200

    Add solution for isograms.rb

---
+++ Ruby/7kyu/isograms.rb
@@ -0,0 +1,21 @@
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+#
+# is_isogram("Dermatoglyphics" ) == true
+# is_isogram("aba" ) == false
+# is_isogram("moOse" ) == false # -- ignore letter case
+#
+# def is_isogram(string)
+#   #your code here
+# end
+
+# My solution
+
+def is_isogram(string)
+  string.downcase.split('') == string.downcase.split('').uniq
+end
+
+# The most popular solution
+
+# def is_isogram(string)
+#   string.downcase.chars.uniq == string.downcase.chars
+# end
