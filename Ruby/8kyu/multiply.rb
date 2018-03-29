fb5637fa32dee5486d5420a141b623760a7aa52d
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 13:23:29 +0200

    Add solution for multiply.rb

---
+++ Ruby/8kyu/multiply.rb
@@ -0,0 +1,17 @@
+# The code does not execute properly. Try to figure out why.
+
+# def multiply(a b)
+#   a * b
+# end
+
+# My solution
+
+def multiply(a, b)
+  a * b
+end
+
+# The most popular solution
+
+def multiply(a, b)
+  a * b
+end
