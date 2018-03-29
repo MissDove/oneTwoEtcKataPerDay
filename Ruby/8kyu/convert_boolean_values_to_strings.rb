9df7506101521aff700b343f372941b734c0e89a
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-29 19:33:58 +0200

    Add solution for convert_boolean_values_to_strings.rb

---
+++ Ruby/8kyu/convert_boolean_values_to_strings.rb
@@ -0,0 +1,17 @@
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+#
+# def bool_to_word bool
+#   # TODO
+# end
+
+# My solution
+
+def bool_to_word bool
+  bool == true ? 'Yes' : 'No'
+end
+
+# The most popular solution
+
+# def bool_to_word bool
+#   bool ? 'Yes' : 'No'
+# end
