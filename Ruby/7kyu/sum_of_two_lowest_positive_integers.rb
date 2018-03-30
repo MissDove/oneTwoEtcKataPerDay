f73c3381bc2f3cbb6355aab553d5363e81e2569b
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-30 21:06:44 +0200

    Add solution for sum_of_two_lowest_positive_integers.rb

---
+++ Ruby/7kyu/sum_of_two_lowest_positive_integers.rb
@@ -0,0 +1,23 @@
+# Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 integers. No floats or empty arrays will be passed.
+#
+# For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+#
+# [10, 343445353, 3453445, 3453545353453] should return 3453455.
+#
+# Hint: Do not modify the original array.
+#
+# def sum_two_smallest_numbers(numbers)
+#   #Your code here
+# end
+
+# My solution
+
+def sum_two_smallest_numbers(numbers)
+  numbers.sort[0] + numbers.sort[1]
+end
+
+# The most popular solution
+# 
+# def sum_two_smallest_numbers(numbers)
+#   numbers.min(2).reduce(:+)
+# end
