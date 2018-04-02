614292aed11cbbfa9c7216038a4d3fc2b48370de
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-04-02 22:53:31 +0200

    Add solution for oppositeNumber.js

---
+++ JavaScript/8kyu/oppositeNumber.js
@@ -0,0 +1,25 @@
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+//
+// 1: -1
+// 14: -14
+// -34: 34
+//
+// But can you do it in 1 line of code and without any conditionals?
+//
+// function opposite(number) {
+//   //your code here
+// }
+
+// My solution
+
+function opposite(number) {
+  return -number;
+}
+
+// The most popular solution
+
+function opposite(number) {
+  return(-number);
+}
