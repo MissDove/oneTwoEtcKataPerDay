ffa1772f7ff0599da881026e747eb09f1be72bfd
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-04-08 21:20:25 +0100

    Add solution for convertANumberToAString.js

---
+++ JavaScript/8kyu/convertANumberToAString.js
@@ -0,0 +1,23 @@
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+numberToString(123); // returns '123';`
+numberToString(999); // returns '999';`
+
+function numberToString(num) {
+  // Return a string of the number here!
+}
+
+// My solution
+
+function numberToString(num) {
+  return num.toString();
+}
+
+// The most popular solution
+// 
+// function numberToString(num) {
+//   return num.toString();
+// }
