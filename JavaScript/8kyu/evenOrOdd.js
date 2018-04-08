36a0115bf37ba6057ab614da7077c106cede1f17
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-04-08 21:34:12 +0100

    Add solution for evenOrOdd.js

---
+++ JavaScript/8kyu/evenOrOdd.js
@@ -0,0 +1,17 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//
+// function even_or_odd(number) {
+//   // ...
+// }
+
+// My solution
+
+function even_or_odd(number) {
+  return number%2 == 0 ? "Even" : "Odd";
+}
+
+// The most popular solution
+// 
+// function even_or_odd(number) {
+//   return number % 2 ? "Odd" : "Even"
+// }
