8eb1eff174ce5de2a1f817c7603d277950f502dc
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-04-08 16:21:49 +0100

    Add solution for returningStrings.js

---
+++ JavaScript/8kyu/returningStrings.js
@@ -0,0 +1,19 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+//
+// function greet(name){
+//   //your code here
+// }
+
+// My solution
+
+function greet(name){
+  return "Hello, " + name + " how are you doing today?";
+}
+
+// The most popular solution
+//
+// function greet(name){
+//   return `Hello, ${name} how are you doing today?`;
+// }
