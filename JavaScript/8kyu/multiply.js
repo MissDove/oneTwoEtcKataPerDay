25be8d0ad5c61b7af1ce778d12894329dc7d88bf
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-04-08 17:24:34 +0100

    Add solution for multiply.js

---
+++ JavaScript/8kyu/multiply.js
@@ -0,0 +1,17 @@
+// The code does not execute properly. Try to figure out why.
+//
+// function multiply(a, b){
+//   a * b
+// }
+
+// My solution
+
+function multiply(a, b){
+  return a * b;
+}
+
+//  The most popular solution
+//
+// multiply = function (a, b) {
+//   return a * b;
+// }
