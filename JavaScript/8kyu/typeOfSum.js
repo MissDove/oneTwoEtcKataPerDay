5637f03980f34e647c6747ea782793a35c21888a
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-04-21 23:34:42 +0100

    Add solution for typeOfSum.js

---
+++ JavaScript/8kyu/typeOfSum.js
@@ -0,0 +1,15 @@
+// Return the type of the sum of the two arguments.
+//
+// function typeOfSum(a, b) {
+//   // good luck
+// }
+
+// My solution
+
+function typeOfSum(a, b) {
+  return typeof(a + b);
+}
+
+// The most popular solution
+// 
+// const typeOfSum = (a, b) => typeof(a + b);
