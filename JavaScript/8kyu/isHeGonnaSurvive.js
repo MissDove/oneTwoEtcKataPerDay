bfd8dae6878035e96c1eed03ce161b1ef8795edd
Agata Golebiewska <agata.golebiewska@telegraph.co.uk>
2018-12-09 20:51:41 +0000

    Add solution for isHeGonnaSurvive.js

---
+++ JavaScript/8kyu/isHeGonnaSurvive.js
@@ -0,0 +1,26 @@
+// A hero is on his way to the castle to complete
+// his mission. However, he's been told that the castle
+// is surrounded with a couple of powerful dragons!
+// Each dragon takes 2 bullets to be defeated, our hero
+// has no idea how many bullets he should carry.
+// Assuming he's gonna grab a specific given number of
+// bullets and move forward to fight another specific
+// given number of dragons, will he survive?
+
+// Return True if yes, False otherwise :)
+
+// function hero(bullets, dragons){
+//     //Get Coding!
+// }
+
+// MY SOLUTION
+
+function hero(bullets, dragons) {
+  return bullets / dragons >= 2 ? true : false;
+}
+
+// MOST POPULAR SOLUTION
+
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2;
+}
